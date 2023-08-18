f3abbdf5b90afb4a1acf7cf96314027f6678a107
DoyunKim <louiskim.dev@gmail.com>
2023-08-18 19:52:35 +0900

    1. lat, lng -> floatfield 2. add healthcheck

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("map/", include("map.urls")),
     path('user/', include('accounts.urls')),
+    path("healthcheck", views.HealthCheck.as_view()),
 ]
